16a6f5e9bf8beff485c8ba4343dd102282494a2c
Andreas Johansson <Andreas.cm.johansson@gmail.com>
2016-03-25 23:02:00 +0100

    Fixat saker

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -47,3 +47,35 @@
         (recur remaining
                (into final-body-parts
                      (set [part (matching-part part)])))))))
+
+(defn my-reduce
+  ([f initial coll]
+   (loop [result initial
+          remaining coll]
+     (if (empty? remaining)
+       result
+       (recur (f result (first remaining)) (rest remaining)))))
+  ([f [head & tail]]
+   (my-reduce f head tail)))
+
+(defn better-symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (matching-part part)])))
+          []
+          asym-body-parts))
+
+(defn extra-part [part]
+  (let [name (:name part)]
+    (if (or (clojure.string/includes? name "left") (clojure.string/includes? name "right"))
+      {:name (clojure.string/join ["second-" name]) :size (:size part)}
+      part)))
+
+(defn expand-body-parts
+  "Wooh"
+  [body-parts]
+  (reduce (fn [final-body-parts part]
+            (into final-body-parts (set [part (extra-part part)])))
+          []
+          body-parts))
