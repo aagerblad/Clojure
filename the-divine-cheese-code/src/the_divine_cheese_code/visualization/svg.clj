6cb7ce185bc35fb5f793277bef6e3af027cb9b40
Andreas Johansson <Andreas.cm.johansson@gmail.com>
2016-06-12 21:40:25 +0200

    Slow and steady

---
+++ the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -0,0 +1,39 @@
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))
+                 ks))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
+
+
+
+;(ns the-divine-cheese-code.visualization.svg
+;  (:require [clojure.string :as s])
+;  (:refer-clojure :exclude [min max]))
+;
+;(defn comparator-over-maps
+;  [comparison-fn ks]
+;   (fn [maps]
+;     (zipmap ks
+;             (map (fn [k] (apply comparison-fn (map k maps)))
+;                  ks))))
+;
+;(def smin (comparator-over-maps clojure.core/min [:lat :lng]))
+;(def smax (comparator-over-maps clojure.core/max [:lat :lng]))
+
+(defn latlng->point
+  "Convert lat/lng map to comma-separated string"
+  [latlng]
+  (str (:lat latlng) "," (:lng latlng)))
+
+(defn points
+  [locations]
+  (clojure.string/join " " (map latlng->point locations)))
+
